47e54873cb88b5d23fbd4cfd960dee85c30fd121
实验室 <875241454@qq.com>
2023-08-18 21:12:47 +0800

    2023/8/18

---
+++ 6.栈与队列/6.8前 K 个高频元素/try.cpp
@@ -0,0 +1,69 @@
+// 347. 前 K 个高频元素
+// 给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+
+// 示例 1:
+// 输入: nums = [1,1,1,2,2,3], k = 2
+// 输出: [1,2]
+
+// 示例 2:
+// 输入: nums = [1], k = 1
+// 输出: [1]
+
+// 提示：
+
+// 1 <= nums.length <= 105
+// k 的取值范围是 [1, 数组中不相同的元素的个数]
+// 题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的
+
+// 进阶：你所设计算法的时间复杂度 必须 优于 O(n log n) ，其中 n 是数组大小。
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    class cmp {
+    public:
+        bool operator()(const pair<int,int>& a, const pair<int,int>& b){
+            // 注意：大于小顶堆，小于大顶堆
+            return a.second > b.second;
+        }
+    };
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> map;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, cmp> q;
+        vector<int> res;
+        for(int i: nums){
+            map[i]++;
+        }
+        for(pair<int,int> m: map){
+            // cout<<m.first<<":"<<m.second<<endl;
+            q.push(m);
+            if(q.size() > k){
+                q.pop();
+            }
+        }
+        while(!q.empty()){
+            res.push_back(q.top().first);
+            q.pop();
+        }
+        return res;
+    }
+};
+
+int main(){
+    vector<int> nums{1,1,1,2,2,3};
+    int k = 2;
+    Solution s;
+    vector<int> res = s.topKFrequent(nums, k);
+    for(int i: res){
+        cout<<i<<" ";
+    }
+    system("pause");
+    return 0;
+}
