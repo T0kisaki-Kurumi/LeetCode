742364a64f7d6b2871545240a916e07a65ae2b1b
实验室 <875241454@qq.com>
2023-10-18 11:02:27 +0800

    2023/10/18

---
+++ 10.动态规划/10.32买卖股票的最佳时机/try.cpp
@@ -0,0 +1,56 @@
+// 121. 买卖股票的最佳时机
+// 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+
+// 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+
+// 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+
+// 示例 1：
+// 输入：[7,1,5,3,6,4]
+// 输出：5
+// 解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+//      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+
+// 示例 2：
+// 输入：prices = [7,6,4,3,1]
+// 输出：0
+// 解释：在这种情况下, 没有交易完成, 所以最大利润为 0。
+
+// 提示：
+
+// 1 <= prices.length <= 105
+// 0 <= prices[i] <= 104
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size()==1) return 0;
+        vector<vector<int>> dp(prices.size(), vector<int>{0,0});
+        dp[0][1] = -prices[0]; //0不持有  1持有
+        for(int i=1; i<prices.size(); ++i){
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i]);
+            // dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i]);
+            dp[i][1] = max(dp[i-1][1], -prices[i]); //从不持有转为持有的时候拥有的钱一定是0，保证了只买入一次
+        }
+        // for(int i=0; i<prices.size(); i++){
+        //     for(int j=0; j<2; j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return max(dp.back()[0], dp.back()[1]);
+    }
+};
+
+int main(){
+    vector<int> nums{7,1,5,3,6,4};
+    Solution sol;
+    cout<<sol.maxProfit(nums);
+    system("pause");
+    return 0;
+}
