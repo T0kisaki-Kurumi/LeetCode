d0e793c3dcea921ad4c49f6d1c773fcf07e0ba67
T0kisaki-Kurumi <875241454@qq.com>
2023-11-05 22:10:16 +0800

    2023/11/5

---
+++ 2.链表/2.4翻转链表/two.cpp
@@ -0,0 +1,57 @@
+// 206. 反转链表
+
+// 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+
+// 示例 1：
+
+// 输入：head = [1,2,3,4,5]
+// 输出：[5,4,3,2,1]
+
+
+// 示例 2：
+
+// 输入：head = [1,2]
+// 输出：[2,1]
+// 示例 3：
+
+// 输入：head = []
+// 输出：[]
+ 
+
+// 提示：
+
+// 链表中节点的数目范围是 [0, 5000]
+// -5000 <= Node.val <= 5000
+ 
+// 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+
+#include "../MyLinkedList.cpp"
+#include <vector>
+using namespace std;
+
+class Solution{
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == nullptr) return head;
+        ListNode* pre = nullptr;
+        ListNode* cur = head;
+        while(cur){
+            ListNode* tmp = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+};
+
+int main(){
+    vector<int> a{1,2,3,4,5};
+    ListNode* head = testVector2LinkedList(a);
+    printTestLinkedList(head);
+    Solution s;
+    ListNode* newHead = s.reverseList(head);
+    printTestLinkedList(newHead);
+    system("pause");
+    return 0;
+}
---
+++ 2.链表/2.5两两交换链表中的节点/two.cpp
@@ -0,0 +1,59 @@
+// 24. 两两交换链表中的节点
+// 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
+
+ 
+
+// 示例 1：
+
+
+// 输入：head = [1,2,3,4]
+// 输出：[2,1,4,3]
+// 示例 2：
+
+// 输入：head = []
+// 输出：[]
+// 示例 3：
+
+// 输入：head = [1]
+// 输出：[1]
+ 
+
+// 提示：
+
+// 链表中节点的数目在范围 [0, 100] 内
+// 0 <= Node.val <= 100
+
+#include "../MyLinkedList.cpp"
+#include <vector>
+using namespace std;
+
+class Solution{
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head == nullptr) return head;
+        ListNode* dummyHead = new ListNode(0, head);
+        ListNode* cur = head;
+        ListNode* pre = dummyHead;
+        while(cur && cur->next){
+            pre->next = cur->next;
+            cur->next = pre->next->next;
+            pre->next->next = cur;
+            pre = cur;
+            cur = cur->next;
+        }
+        ListNode* newHead = dummyHead->next;
+        delete dummyHead;
+        return newHead;
+    }
+};
+
+int main(){
+    vector<int> a{1,2,3,4,5};
+    ListNode* head = testVector2LinkedList(a);
+    printTestLinkedList(head);
+    Solution s;
+    ListNode* newHead = s.swapPairs(head);
+    printTestLinkedList(newHead);
+    system("pause");
+    return 0;
+}
