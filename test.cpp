2f3203cc90aa6f9174e271d8137293e4a517b1fc
实验室 <875241454@qq.com>
2024-03-11 11:42:03 +0800

    2024/3/11

--- test.cpp
+++ test.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cstring>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 using namespace std;
 
 int main(){
-    char * a = "12345";
-    char * b;
-    // memcpy(b, a, strlen(a) + 1);
-    strcpy(b,a);
-    cout<<strlen(b)<<endl;
+    // const char* a = "54321 6789";
+    // int fd = open("E:\\code\\LeetCode\\LeetCode\\test.txt", O_RDWR);
+    // if(fd < 0){
+    //     perror("open");
+    //     system("pause");
+    //     return 1;
+    // }
+    // // int w = write(fd, a, strlen(a));
+    // int w = write(fd, a, sizeof(a));
+    // cout<<w<<endl;
+    // close(fd);
+    char buf[1024] = {0};
+    cout<<sizeof(buf)<<endl;
 
     system("pause");
 }
