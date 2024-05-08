96feca02c1eb74354678c991f49957cf2f2163cb
T0kisaki-Kurumi <875241454@qq.com>
2024-05-08 18:13:01 +0800

    2024/5/8

--- test3.cpp
+++ test3.cpp
@@ -3,31 +3,8 @@
 #include <unordered_set>
 using namespace std;
 
-unordered_set<long long> uset;
-
 int main(){
-    int q;
-    cin>>q;
-    while(q--){
-        uset.clear();
-        long long k,a,b,l,r;
-        cin>>k>>a>>b>>l>>r;
-        long long res = k;
-        int count = 0;
-        for(; res<=r; res*=a){
-            if(uset.find(res) != uset.end()) continue;
-            long long tmp = res;
-            for(; tmp<=r; tmp*=b){
-                if(l<=tmp){
-                    if(uset.find(tmp)==uset.end()){
-                        uset.insert(tmp);
-                        ++count;
-                    }
-                }
-            }
-        }
-        cout<<count<<endl;
-    }
+    
 
     system("pause");
     return 0;
