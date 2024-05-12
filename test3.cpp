ac322fcf6aab677b7b2d42fba085840270b0b03d
T0kisaki-Kurumi <875241454@qq.com>
2024-05-12 20:19:16 +0800

    2024/5/12

--- test3.cpp
+++ test3.cpp
@@ -6,11 +6,6 @@ using namespace std;
 int main(){
 <<<<<<< HEAD
     
-=======
-    string s = "abcdefa\
-    aaaa";
-    cout<<s<<endl;
->>>>>>> 9a5334f478d7c112a17e6fa630f4a844b5a11df3
 
     system("pause");
     return 0;
